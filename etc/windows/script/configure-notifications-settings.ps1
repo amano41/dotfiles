28cfc44d9759ae6b9c8528388f02c362164a1473
Amano, Yoichi <amano.yoichi@gmail.com>
2020-09-16 12:08:04 +0900

    Add configure-notifications-settings.ps1

---
+++ etc/windows/script/configure-notifications-settings.ps1
@@ -0,0 +1,24 @@
+Write-Host "Configuring Notifications Settings..."
+
+
+try {
+	Get-Command -Name "Set-Registry" -ErrorAction Stop | Out-Null
+}
+catch {
+	$path = Split-Path -Parent $MyInvocation.MyCommand.Path
+	$path = Join-Path $path "registry-helper.ps1"
+	. $path
+}
+
+
+## ロック画面に通知を表示する
+Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings" "NOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK" 0
+
+## ロック画面にリマインダーと VolP の着信を表示する
+Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings" "NOC_GLOBAL_SETTING_ALLOW_CRITICAL_TOASTS_ABOVE_LOCK" 0
+
+## Windows へようこその情報を表示する
+Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" "SubscribedContent-310093Enabled" 0
+
+## Windows を使用するためのヒントやおすすめの方法を取得
+Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" "SoftLandingEnabled" 0
--- etc/windows/setup.ps1
+++ etc/windows/setup.ps1
@@ -55,19 +55,8 @@ if (!([Security.Principal.WindowsPrincipal]`
 . "./script/configure-personalization-settings.ps1"
 
 
-####################  設定 → システム → 通知とアクション  ####################
-
-## ロック画面に通知を表示する
-Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings" "NOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK" 0
-
-## ロック画面にリマインダーと VolP の着信を表示する
-Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings" "NOC_GLOBAL_SETTING_ALLOW_CRITICAL_TOASTS_ABOVE_LOCK" 0
-
-## Windows へようこその情報を表示する
-Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" "SubscribedContent-310093Enabled" 0
-
-## Windows を使用するためのヒントやおすすめの方法を取得
-Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" "SoftLandingEnabled" 0
+## 設定 → システム → 通知とアクション
+. "./script/configure-notifications-settings.ps1"
 
 
 ####################  設定 → プライバシー → Windows のアクセス許可  ####################
