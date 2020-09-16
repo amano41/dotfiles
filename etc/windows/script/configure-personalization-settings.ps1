6ffb0af75f07f16058e2f0256a4b0a54ff228241
Amano, Yoichi <amano.yoichi@gmail.com>
2020-09-16 11:51:55 +0900

    Add configure-personalization-settings.ps1

---
+++ etc/windows/script/configure-personalization-settings.ps1
@@ -0,0 +1,27 @@
+Write-Host "Configuring Personalization Settings..."
+
+
+try {
+	Get-Command -Name "Set-Registry" -ErrorAction Stop | Out-Null
+}
+catch {
+	$path = Split-Path -Parent $MyInvocation.MyCommand.Path
+	$path = Join-Path $path "registry-helper.ps1"
+	. $path
+}
+
+
+## ロック画面：ロック画面にトリビアやヒントなどの情報を表示する
+Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" "RotatingLockScreenOverlayEnabled" 0
+
+## スタート：よく使うアプリを表示する
+Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "Start_TrackProgs" 0
+
+## スタート：ときどきスタートメニューにおすすめのアプリを表示する
+Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" "SystemPaneSuggestionsEnabled" 0
+
+## スタート：スタートメニューまたはタスクバーのジャンプリストに最近開いた項目を表示する
+Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "Start_TrackDocs" 0
+
+## タスクバー：タスクバーをすべてのディスプレイに表示する
+Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "MMTaskbarEnabled" 0
--- etc/windows/setup.ps1
+++ etc/windows/setup.ps1
@@ -51,22 +51,8 @@ if (!([Security.Principal.WindowsPrincipal]`
 . "./script/setup-explorer.ps1"
 
 
-####################  設定 → 個人用設定  ####################
-
-## ロック画面：ロック画面にトリビアやヒントなどの情報を表示する
-Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" "RotatingLockScreenOverlayEnabled" 0
-
-## スタート：よく使うアプリを表示する
-Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "Start_TrackProgs" 0
-
-## スタート：ときどきスタートメニューにおすすめのアプリを表示する
-Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" "SystemPaneSuggestionsEnabled" 0
-
-## スタート：スタートメニューまたはタスクバーのジャンプリストに最近開いた項目を表示する
-Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "Start_TrackDocs" 0
-
-## タスクバー：タスクバーをすべてのディスプレイに表示する
-Set-Registry "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "MMTaskbarEnabled" 0
+## 設定 → 個人用設定
+. "./script/configure-personalization-settings.ps1"
 
 
 ####################  設定 → システム → 通知とアクション  ####################
