355fa38752d9e5a1c2c4bcd08c2f9b350084fe58
Amano, Yoichi <amano.yoichi@gmail.com>
2020-09-28 20:02:50 +0900

    Update WSL to version 2

--- etc/windows/script/setup-wsl.ps1
+++ etc/windows/script/setup-wsl.ps1
@@ -1,3 +1,6 @@
 Write-Host "Setting up WSL..." -ForegroundColor Magenta
 
-Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName VirtualMachinePlatform -Online -All -NoRestart
+
+wsl --set-default-version 2
