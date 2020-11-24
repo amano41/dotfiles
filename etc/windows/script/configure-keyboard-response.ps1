b0b8a76b121f732bb3519db589df8003e504906e
Amano, Yoichi <amano.yoichi@gmail.com>
2020-11-24 16:51:43 +0900

    Tweak keyboard response

--- etc/windows/script/configure-keyboard-response.ps1
+++ etc/windows/script/configure-keyboard-response.ps1
@@ -2,8 +2,8 @@ Write-Host "Configuring Keyboard Response..." -ForegroundColor Magenta
 
 $path = "HKCU:\Control Panel\Accessibility\Keyboard Response"
 
-Set-ItemProperty $path -Name "AutoRepeatDelay" -Type String -Value "200"
-Set-ItemProperty $path -Name "AutoRepeatRate" -Type String -Value "16"
+Set-ItemProperty $path -Name "AutoRepeatDelay" -Type String -Value "300"
+Set-ItemProperty $path -Name "AutoRepeatRate" -Type String -Value "50"
 Set-ItemProperty $path -Name "BounceTime" -Type String -Value "0"
 Set-ItemProperty $path -Name "DelayBeforeAcceptance" -Type String -Value "0"
 Set-ItemProperty $path -Name "Flags" -Type String -Value "59"
