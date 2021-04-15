9552376fbd6678a93d4ae4b388699062f263041e
Amano, Yoichi <amano.yoichi@gmail.com>
2021-04-16 00:08:20 +0900

    Add check for existence of wsl command

--- etc/windows/script/setup-wsl.ps1
+++ etc/windows/script/setup-wsl.ps1
@@ -3,4 +3,10 @@ Write-Host "Setting up WSL..." -ForegroundColor Magenta
 Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -All -NoRestart
 Enable-WindowsOptionalFeature -FeatureName VirtualMachinePlatform -Online -All -NoRestart
 
-wsl --set-default-version 2
+if (Get-Command -Name "wsl" -ErrorAction SilentlyContinue) {
+	wsl --set-default-version 2
+}
+else {
+	Write-Warning "WSL not installed."
+	Write-Warning "You need to run 'wsl --set-default-version 2' after installing WSL."
+}
