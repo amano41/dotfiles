d16dd13bb647dbe5411ae7e97e6dae7821e416b7
Amano, Yoichi <amano.yoichi@gmail.com>
2021-04-16 00:23:39 +0900

    Add check for existence of pip command

--- etc/windows/script/install-python-packages.ps1
+++ etc/windows/script/install-python-packages.ps1
@@ -1,12 +1,21 @@
 Write-Host "Installing Python packages..." -ForegroundColor Magenta
 
-If (!(Get-Command -Name "pip" -ErrorAction SilentlyContinue)) {
-	Write-Error "pip not found." -ErrorAction Stop
+
+If (Get-Command -Name "pip" -ErrorAction SilentlyContinue) {
+	$pip = "pip"
+}
+else {
+	$pip = Join-Path $env:USERPROFILE "scoop/apps/python/current/Scripts/pip"
+	if (!(Test-Path $pip)) {
+		Write-Error "pip not found." -ErrorAction Stop
+	}
 }
 
+
 function install($package) {
-	pip --disable-pip-version-check install --user --no-warn-script-location $package | Select-String 'Requirement already satisfied' -NotMatch
+	& $pip --disable-pip-version-check install --user --no-warn-script-location $package | Select-String 'Requirement already satisfied' -NotMatch
 }
+
 
 ## atom: linter-flake8
 install flake8
