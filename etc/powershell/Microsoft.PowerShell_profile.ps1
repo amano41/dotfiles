122270dfd2156bc28ef8c122c12b035c930b75ec
Amano, Yoichi <amano.yoichi@gmail.com>
2021-10-25 17:24:08 +0900

    Move utility functions to utils.ps1

--- etc/powershell/Microsoft.PowerShell_profile.ps1
+++ etc/powershell/Microsoft.PowerShell_profile.ps1
@@ -57,62 +57,7 @@ Set-PSReadLineOption -WordDelimiters ";:,.[]{}()/\|^&*-=+'`" !?@#$%&_<>「」（
 # ヘルパー関数
 ##################################################
 
-# 管理者権限があるかチェックする関数
-function Test-Privilege {
-	return ([Security.Principal.WindowsPrincipal]`
-	        [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-	        [Security.Principal.WindowsBuiltInRole] "Administrator")
-}
-
-# シンボリックリンクを作成する関数
-function Symlink($src, $dest) {
-
-	if (Test-Path $dest -PathType Container) {
-		$type = Get-Item $dest | Select-Object -ExpandProperty LinkType
-		if ($type -ne "SymbolicLink") {
-			Write-Warning "'$dest' already exists."
-			$backup = "$dest" + "." + (Get-Date -UFormat "%Y%m%d%H%M%S")
-			Move-Item $dest $backup
-		}
-	}
-	Write-Host "----"
-
-	$path = Split-Path -Parent $dest
-	$name = Split-Path -Leaf $dest
-
-	if (!(Test-Path $path)) {
-		New-Item $path -ItemType Directory
-	}
-
-	New-Item -ItemType SymbolicLink -Path $path -Name $name -Target $src -Force
-}
-
-# 上書きせずにコピーする関数
-function SafeCopy($src, $dest) {
-
-	if (Test-Path $dest) {
-		Write-Warning "'$dest' already exists."
-		$backup = "$dest" + "." + (Get-Date -UFormat "%Y%m%d%H%M%S")
-		Move-Item $dest $backup
-	}
-
-	$path = Split-Path -Parent $dest
-
-	if (!(Test-Path $path)) {
-		New-Item $path -ItemType Directory
-	}
-
-	Copy-Item $src $dest
-}
-
-# ショートカットを作成する関数
-function New-Shortcut($src, $dest, $opts = "") {
-	$wsh = New-Object -ComObject WScript.Shell
-	$lnk = $wsh.CreateShortcut($dest)
-	$lnk.TargetPath = $src
-	$lnk.Arguments = $opts
-	$lnk.Save()
-}
+. "$env:USERPROFILE\dotfiles\etc\powershell\utils.ps1"
 
 
 ##################################################
---
+++ etc/powershell/utils.ps1
@@ -0,0 +1,53 @@
+# 管理者権限があるかチェックする関数
+function Test-Privilege {
+	return ([Security.Principal.WindowsPrincipal]`
+	        [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+	        [Security.Principal.WindowsBuiltInRole] "Administrator")
+}
+
+
+# シンボリックリンクを作成する関数
+function New-Symlink($src, $dest) {
+
+	# 管理者権限の確認
+	if (!(Test-Privilege)) {
+		Write-Error "This command need to be run with elevated privileges." -ErrorAction Stop
+	}
+
+	if (!(Test-Path $src)) {
+		Write-Error "No such file or directory: $src" -ErrorAction Stop
+	}
+
+	$src = Resolve-Path $src
+
+	if (Test-Path $dest -PathType Container) {
+		$type = Get-Item $dest | Select-Object -ExpandProperty LinkType
+		if ($type -ne "SymbolicLink") {
+			$name = Split-Path -Leaf $src
+			$dest = Join-Path $dest $name
+		}
+	}
+
+	$path = Split-Path -Parent $dest
+	$name = Split-Path -Leaf $dest
+
+	if (!($path)) {
+		$path = "."
+	}
+
+	if (!(Test-Path $path)) {
+		New-Item -Path $path -ItemType Directory
+	}
+
+	New-Item -ItemType SymbolicLink -Path $path -Name $name -Target $src -Force | Out-Null
+}
+
+
+# ショートカットを作成する関数
+function New-Shortcut($src, $dest, $opts = "") {
+	$wsh = New-Object -ComObject WScript.Shell
+	$lnk = $wsh.CreateShortcut($dest)
+	$lnk.TargetPath = $src
+	$lnk.Arguments = $opts
+	$lnk.Save()
+}
