e8c1c5df133a7b16063fe5ac1b5934e64f11e9fb
Amano, Yoichi <amano.yoichi@gmail.com>
2021-11-04 15:17:33 +0900

    Remove WslInterop

--- etc/windows/script/install-powershell-modules.ps1
+++ etc/windows/script/install-powershell-modules.ps1
@@ -6,7 +6,6 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 # PSGallery を信頼するリポジトリに追加
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
-Install-Module WslInterop
 Install-Module PSFzf
 
 # PSGallery を信頼するリポジトリから削除
