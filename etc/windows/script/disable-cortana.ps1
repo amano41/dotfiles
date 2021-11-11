bd75c5e7fcd59754dc4d6130319b4565b15bde99
Amano, Yoichi <amano.yoichi@gmail.com>
2021-11-11 18:23:05 +0900

    Cosmetic: disable-cortana.ps1

--- etc/windows/script/disable-cortana.ps1
+++ etc/windows/script/disable-cortana.ps1
@@ -1,9 +1,10 @@
 Write-Host "Disabling Cortana..." -ForegroundColor Magenta
 
 $path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+$name = "AllowCortana"
 
 if (!(Test-Path $path)) {
 	New-Item -Path $path -Force | Out-Null
 }
 
-Set-ItemProperty -Path $path -Name "AllowCortana" -Type DWord -Value 0
+Set-ItemProperty -Path $path -Name $name -Type DWord -Value 0
