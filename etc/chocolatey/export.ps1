e43d1f7b3308516074b8ea5e9b223811e334342e
Yoichi Amano <amano.yoichi@gmail.com>
2024-07-26 15:14:37 +0900

    Remove chocolatey setup scripts

--- etc/chocolatey/export.ps1
+++
@@ -1,3 +0,0 @@
-$body = ''
-choco list -lo -r | % { $_ -split '\|' | select -first 1 } | % { $body += "`n`t<package id=""$_"" />" }
-Set-Content packages.config "<?xml version=`"1.0`" encoding=`"utf-8`"?>`n<packages>$body`n</packages>"
