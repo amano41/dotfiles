f2439a27490b76e39cc7b32afd0781f242c50ccb
Yoichi Amano <amano.yoichi@gmail.com>
2024-07-26 15:13:35 +0900

    Remove windows setup scripts

--- etc/windows/script/hide-removable-drives.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Hiding Removable Drives from Navigation Pane..." -ForegroundColor Magenta
-
-$path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\DelegateFolders\{F5FB2C77-0E2F-4A16-A381-3E560C68BC83}"
-
-if (Test-Path $path) {
-	Remove-Item -Path $path
-}
